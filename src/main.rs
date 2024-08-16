08dee102210075e85131fb757f8afd845f5dc3c7
贺俊浩 <nougat_hx@qq.com>
2024-08-16 17:25:07 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::{io::BufReader, process::Command, thread};
+
+use serde_json::Value;
+struct ExcCommand {
+    name: String,
+    content: String,
+}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut commands: Vec<ExcCommand> = vec![];
+    let config_path = "config.json";
+    let config = read_config(config_path)?;
+    if let Value::Object(map) = config {
+        for (key, value) in map {
+            let command = ExcCommand {
+                name: key.clone(),
+                content: value.as_str().unwrap_or("").to_string(),
+            };
+            commands.push(command);
+        }
+    }
+    for command in commands{
+        let handle = thread::spawn(move || {
+            println!("{}: {}", command.name, command.content);
+            Command::new("osascript")
+            .arg("-e")
+            .arg(format!("tell application \"Terminal\" to do script \"{}\"", command.content))
+            .spawn()
+        });
+        handle.join().unwrap()?;
+    }
+    Ok(())
+}
+pub fn read_config<P: AsRef<std::path::Path>>(
+    path: P,
+) -> Result<Value, Box<dyn std::error::Error>> {
+    let file = std::fs::File::open(path)?;
+    let reader = BufReader::new(file);
+    let config = serde_json::from_reader(reader)?;
+    Ok(config)
+}
